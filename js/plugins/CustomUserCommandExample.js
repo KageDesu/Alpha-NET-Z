de61c75f04e055cf4261416e79d48c5a4b0630c7
Pheonix KageDesu <pheonix17@mail.ru>
2021-04-16 11:34:21 +0300

    Добавил пример CustomCommand кода

---
+++ js/plugins/CustomUserCommandExample.js
@@ -0,0 +1,21 @@
+// FOR PLUGIN DEVELOPERS
+
+// 1. SEND CUSTOM COMMAND
+var data = {}; // * some data if you need it (or null)
+nAPI.sendCustomCommand("someName", data);
+
+// 2. RECEIVE AND WORK WITH YOUR CUSTOM COMMAND
+
+//@[ALIAS] // Aliasing is REQUIRED!
+var _alias_nAPI_onCustomCommand = nAPI.onCustomCommand;
+nAPI.onCustomCommand = function (name, data) {
+    _alias_nAPI_onCustomCommand.call(this, ...arguments);
+    if(name == "someName") {
+        // YOUR CODE
+    }
+};
+
+
+// MORE INFO IN GUIDE:
+
+
